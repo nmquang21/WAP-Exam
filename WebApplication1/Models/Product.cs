cfaf7effd7c62772c466d54172f50750d96d82b6
Quang <nmquang21@gmail.com>
2019-11-22 19:35:27 +0700

    Upload.

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        public int UnitInOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public int Discontinued { get; set; }
+        public virtual Category Categoty { get; set; }
+    }
+}
